dbe6dac5b3179a1b4d45fac93f22ac2e8850ba1e
leopoldkwok <leopoldkwok@gmail.com>
2021-03-17 10:15:58 +0000

    Requests

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,6 +5,8 @@ import { GithubContext } from '../context/context';
 
 const Search = () => {
   const [user, setUser] = React.useState('');
+  const { requests } = React.useContext(GithubContext);
+  console.log(requests);
   // get things from global context
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,10 +31,10 @@ const Search = () => {
                 setUser(e.target.value);
               }}
             />
-            <button type='search'>search</button>
+            {requests > 0 && <button type='search'>search</button>}
           </div>
         </form>
-        <h3>requests: 60 / 60</h3>
+        <h3>requests: {requests} / 60</h3>
       </Wrapper>
     </section>
   );
