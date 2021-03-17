6a927ec8cd85025ee07e98aaec3591493e41ac9b
leopoldkwok <leopoldkwok@gmail.com>
2021-03-17 09:13:42 +0000

    Column and Bar Charts

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -57,10 +57,9 @@ const Repos = () => {
     <section className='section'>
       <Wrapper className='section-center'>
         <Pie3D data={mostUsed} />
-        <div></div>
+        <Column3D data={chartData} />
         <Doughnut2D data={mostPopular} />
-        {/* <ExampleChart data={chartData} /> */}
-        <div></div>
+        <Bar3D data={chartData} />
       </Wrapper>
     </section>
   );
